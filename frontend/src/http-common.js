f01b72d3e8c84be2fad35d72255e0c57242cdfa4
EElico <eeyalc@gmail.com>
2023-04-07 22:46:49 +0300

    bsackend on server

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: window.location.href.includes("https") ? "https://main.d395v2jqb4fym4.amplifyapp.com" : "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
